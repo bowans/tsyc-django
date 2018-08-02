8580413ea01be9004daa95dc4923927e934d29a6
bowans <bowans@gmail.com>
2018-08-02 15:57:04 +0800

    Serve uploaded image
    - Add "MEDIA_URL"
    - Refer to https://docs.djangoproject.com/en/2.0/howto/static-files/#serving-files-uploaded-by-a-user-during-development

--- tsyc/urls.py
+++ tsyc/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 import main.views
 import event.views
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', main.views.index, name='index'),
     path('event/', event.views.event, name='event'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
