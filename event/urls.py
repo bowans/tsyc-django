175fecf84812f434f2a8186ae443b2cf971e1894
bowans <bowans@gmail.com>
2018-09-12 16:01:26 +0800

    Add onclick event for event list
    - Pass event id to event list page
    - Handle url for specific event id
    - TODO: implement event detail page

--- event/urls.py
+++ event/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 import event.views
 
 urlpatterns = [
-    path('', event.views.event, name='event'),
+    path('', event.views.event_list, name='event_list'),
+    path('<int:id>/', event.views.event_detail, name='event_detail'),
 ]
--- event/views.py
+++ event/views.py
@@ -1,10 +1,21 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
-from event.models import Event, EventImage
+from event.models import Event
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
 
-
-def event(request):
+def event_list(request):
     events = Event.objects.all().order_by('-date')
-    events_dict = [ {'name': event.name, 'cover': event.images.filter(is_cover=True)[:1]} for event in events]
+    events_dict = [{'name': event.name, 'cover': event.images.filter(is_cover=True)[:1], 'id': event.id} for event in events]
     return render(request, 'event.html', locals())
+
+
+def event_detail(request, id):
+    try:
+        event = Event.objects.get(id=id)
+    except ObjectDoesNotExist:
+        raise Http404
+
+    response = HttpResponse()
+    response.write(event.name)
+    return response
