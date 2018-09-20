234291fc428d9198f3c0c9ed593726e0aa93d69b
bowans <bowans@gmail.com>
2018-09-21 01:23:27 +0800

    Eliminate RuntimeWarning while testing
    - Set correct timezone in settings.py
    - Use pytz.timezone().localize() to get correct time without 6 minutes diff.
      Refer to https://stackoverflow.com/questions/49440018/why-converting-between-time-zones-in-python-yields-different-results

--- event/tests.py
+++ event/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from .models import Event
 
-
 DESCRIPTION_1 = '''歲末年初是與家人好友歡聚的時刻，在聖誕節前夕透過來自世界不同角落的歌曲，一同欣賞如畫的冬景並歡慶聖誕的喜悅。本場曲目包括耳熟能詳的聖誕歌曲以及帶有爵士搖擺風格的精彩彌撒作品，透過傳統與現代的交會，千百年來不變的心意也能充滿無窮的新意。
 
 本團本著對合唱音樂的熱愛，對自我精提升的要求，不斷開創並發掘多元化曲目。期盼在本場《冬日光景》音樂會能以精緻的西方合唱作品和台北市松韻合唱團真摯優美的歌聲，與愛樂者共饗音樂心靈盛宴。'''  # noqa: E501
@@ -27,11 +26,12 @@ DESCRIPTION_2 = '''新加坡的譚秀英（Jennifer Tham）和日本的松下耕
 class EventViewTests(TestCase):
     def setUp(self):
         from datetime import datetime
+        import pytz
         # test object 1
         Event.objects.create(
             name='松韻2017冬季音樂會《冬日光景》',
             description=DESCRIPTION_1,
-            date=datetime(2017, 12, 23, hour=19, minute=30),
+            date=pytz.timezone('Asia/Taipei').localize(datetime(2017, 12, 23, hour=19, minute=30)),
             address='''東吳大學美育中心松怡廳''',
             artist=ARTIST_1,
             music=MUSIC_1,
@@ -43,7 +43,7 @@ class EventViewTests(TestCase):
         Event.objects.create(
             name='《絕境重聲》',
             description=DESCRIPTION_2,
-            date=datetime(2017, 7, 2, hour=14, minute=30))
+            date=pytz.timezone('Asia/Taipei').localize(datetime(2017, 7, 2, hour=14, minute=30)))
 
     def tearDown(self):
         Event.objects.all().delete()
@@ -113,6 +113,7 @@ class EventViewTests(TestCase):
 <p>本次計畫便是希望透過台北的松韻合唱團與逢友合唱團，結合近年來非常活躍的台中室內合唱團、逢甲合唱團與中部合唱中心，集結兩地四團的資源與人力，邀請新加坡國家文化藝術獎（Singapore Cultural Medallion）得主的譚秀英（Jennifer Tam）女士來台訪問，透過交流印尼、馬來西亞與新加坡的當代合唱作品與風格獨具的節目安排（全場14首歌曲，均為台灣首演），為台灣觀眾帶來東南亞地區文化與藝術上的刺激與饗宴。 我們也期待透過合唱音樂與世界知名詩人作品的結合，帶來對生命的反思，從美妙的樂音與深沉溫暖的詩歌中獲得心靈的 安慰與平衡，促進社會和諧。也希望透過舉辦合唱教育交流講座，使台灣的合唱音樂教育與世界接軌，讓更多藝術的美好種子在台灣的土地上成長、茁壯！</p></article>''',  # noqa: E501
             html=True)
         self.assertContains(response, '<h2> 演出日期 </h2>', html=True)
+        self.assertContains(response, '<h3> July 2, 2017, 2:30 p.m. </h3>', html=True)
 
         self.assertNotContains(response, '<h2> 演出地點 </h2>', html=True)
         self.assertNotContains(response, '<h2> 演出者 </h2>', html=True)
